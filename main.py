681b1fe95b5fbfaa9acc44ecbbdb7afea07dcd02
Rana Wahid <rana20479@feng.bu.edu.eg>
2023-10-13 19:53:38 +0300

    Discrete Ploting function

--- main.py
+++ main.py
@@ -1,16 +1,37 @@
-# This is a sample Python script.
+import numpy as np
+import matplotlib.pyplot as plt
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# Open the file and read its contents
+with open("test.txt", "r") as file_object:
+    data = file_object.readlines()
+
+#Discrete plot function
+def discretePlot():
+    # Initialize empty lists to store indices and amplitudes
+    indices = []
+    amplitudes = []
+
+    # Loop through each line in the data
+    for line in data[3:]:
+        parts = line.strip().split()
+        index = int(parts[0])
+        amplitude = float(parts[1])
+        indices.append(index)
+        amplitudes.append(amplitude)
+
+    # Plot the signal
+    plt.figure(figsize=(10, 4))
+    plt.stem(indices, amplitudes)
+    plt.xlabel('Index')
+    plt.ylabel('Amplitude')
+    plt.title('Signal x(n)')
+    plt.grid(True)
+    plt.show()
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+if data[0].strip() == "0":
+    discretePlot()
+elif data[0].strip() == "1":
+    print("Write continous function here")
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCh')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
